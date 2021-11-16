c8ae249236c0e7bcc52c8f103822939989b7eb93
ZeldaPool <smitrana007@gmail.com>
2021-11-15 17:55:21 -0800

    last

--- minHeap.py
+++ minHeap.py
@@ -8,19 +8,18 @@ Smit Rana         -   027980613
 from collections import deque
 
 
-class Node(object):
+class Node:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
 
-class NullNode(object):
-    def __init__(self):
-        self.val = -1
-        self.left = None
-        self.right = None
+class Heap:
+    def __init__(self, heapType):
+        self.heapType = heapType
 
+<<<<<<< Updated upstream
 class OddFilter:
     filtered_arr = []
     
@@ -46,8 +45,23 @@ class OddFilter:
                 self.i += 1
                 return self.input_object[self.i-1]
         return 
+=======
+    def __repr__(self, root):
+        if root is None:
+            return
 
+        queue = deque()
+        queue.append(root)
+        heap_array = []
 
+        while queue:
+
+            curr = queue.popleft()
+>>>>>>> Stashed changes
+
+            heap_array.append(curr.val)
+
+<<<<<<< Updated upstream
 class Heap(Node):
 
     def min_depth(self, root):
@@ -64,6 +78,31 @@ class Heap(Node):
             return self.min_depth(root.left) + 1
 
         return min(self.min_depth(root.left), self.min_depth(root.right)) + 1
+=======
+            if curr.left:
+                queue.append(curr.left)
+
+            if curr.right:
+                queue.append(curr.right)
+
+        return heap_array
+
+    def __str__(self, root):
+        heap_iterator = iter(self.__repr__(root))
+        heap_string = ""
+
+        while True:
+            try:
+                heap_string += str(next(heap_iterator))
+            except StopIteration:
+                break
+
+            heap_string += ", "
+        return heap_string[:len(heap_string)-2]
+
+    def addElement(self, root, val):
+        return self.heapType.append(root, val)
+>>>>>>> Stashed changes
 
     # Returns a list of odd values in the tree in preorder
     def odd_preorder(self, root, result):
@@ -86,37 +125,51 @@ class Heap(Node):
 
         return result
 
+<<<<<<< Updated upstream
     def print_postorder(self, root):
         postorder_result = self.postorder(root, [])
         for i in postorder_result:
             print(i, end=" ")
     
+=======
+>>>>>>> Stashed changes
     # Returns a list of values in inorder
     def inorder(self, root, result):
         if root:
-            result.append(root.val)
             self.inorder(root.left, result)
+            result.append(root.val)
             self.inorder(root.right, result)
 
         return result
 
-    def print_inorder(self, root):
-        inorder_result = self.inorder(root, [])
-        for i in inorder_result:
-            print(i, end=" ")
 
-
+<<<<<<< Updated upstream
 class HeapOperations(Heap) :
         
     minHeap = Heap()
         
+=======
+class HeapOperations:
+    def min_depth(self, root):
+        if root is None:
+            return 0
+        return 1 + max(self.min_depth(root.left), self.min_depth(root.right))
+
+>>>>>>> Stashed changes
     # Inserts value into the min heap and creates node when necessary
     def heap_insert(self, root, val):
         if root is None:
             return Node(val)
 
+<<<<<<< Updated upstream
         left_height = self.minHeap.min_depth(root.left)
         right_height = self.minHeap.min_depth(root.right)
+=======
+        left_height = self.min_depth(root.left)
+        right_height = self.min_depth(root.right)
+
+        temporary_node = val
+>>>>>>> Stashed changes
 
         if val < root.val:
             temporary_node = root.val
@@ -135,6 +188,7 @@ class HeapOperations(Heap) :
 
         return root
 
+<<<<<<< Updated upstream
     def heap_toArray(self, root):
         if root is None:
             return
@@ -173,11 +227,56 @@ class HeapOperations(Heap) :
 
 
 def heap_create():
+=======
+
+class MinHeap(HeapOperations):
+    def isMinOrMaxHeap(self, root, val):
+        return val <= root.val
+
+
+class MaxHeap(HeapOperations):
+    def isMinOrMaxHeap(self, root, val):
+        return val >= root.val
+
+
+class OddFilter:
+    filtered_result = None
+
+    def __init__(self, unfiltered_result):
+        self.unfiltered_result = unfiltered_result
+        self.filter()
+
+    def filter(self):
+        self.filtered_result = list(filter(lambda x: x % 2 == 1, self.unfiltered_result))
+        return self.filtered_result
+
+
+class OddHeapDecorator:
+    filtered_result = None
+    def __init__(self, unfiltered_result, root):
+        self.filtered_result = list(filter(lambda x: x % 2 == 1, unfiltered_result))
+    
+    def __str__(self):
+        result = ""
+
+        for i in self.filtered_result:
+            result += str(i) + ", "
+        
+        return result[:len(result)-2]
+
+    def __repr__(self):
+        return list(self.filtered_result)
+    
+
+def heap_create():
+    heap_object = Heap(heapType=MinHeap())
+>>>>>>> Stashed changes
 
     heapC = HeapOperations()
     heapC2 = Heap()    
        
     root = Node(50)
+<<<<<<< Updated upstream
     heapC.heap_insert(root, 37)
     heapC.heap_insert(root, 24)
     heapC.heap_insert(root, 43)
@@ -194,10 +293,33 @@ def heap_create():
     print(heapC.heap_toString(root))
     print("Inorder:", end=" ")
     heapC2.print_inorder(root)
+=======
+    heap_object.addElement(root, 37)
+    heap_object.addElement(root, 24)
+    heap_object.addElement(root, 43)
+    heap_object.addElement(root, 70)
+    heap_object.addElement(root, 61)
+
+    print("Preorder:", end=" ")
+    print(heap_object.preorder(root, []))
+    print("Postorder:", end=" ")
+    print(heap_object.postorder(root, []))
+    print("ToArray:", end=" ")
+    print(heap_object.__repr__(root))
+    print("ToString:", end=" ")
+    print(heap_object.__str__(root))
+    print("Inorder:", end=" ")
+    print(heap_object.inorder(root, []))
+    unfiltered_arr = heap_object.preorder(root, [])
+    decoTest = OddHeapDecorator(unfiltered_arr, root)
+    print("ToArray OddDecorator:", end=" ")
+    print(decoTest.__repr__())
+    print("ToString OddDecorator:", end=" ")
+    print(decoTest.__str__())
+>>>>>>> Stashed changes
 
 
 if __name__ == "__main__":
-
     heap_create()
 
 
--- test_minHeap.py
+++ test_minHeap.py
@@ -1,25 +1,69 @@
 import unittest
-from minHeap import Node, Heap
+from minHeap import MinHeap, MaxHeap, Node, Heap, OddFilter, OddHeapDecorator
 
 
-class TestMinHeap(unittest.TestCase):
-    def test_minHeap(self):
-        minHeap = Heap()
+class TestHeap(unittest.TestCase):  
+    def test_MinHeap(self):
+        minHeap = Heap(heapType=MinHeap())
 
         # Checks if the root is the smallest element after every insert
         root = Node(50)
-        result = minHeap.insert(root, 30)
-        self.assertEqual(result.val, 30)
-        result = minHeap.insert(root, 20)
-        self.assertEqual(result.val, 20)
-        result = minHeap.insert(root, 70)
-        self.assertEqual(result.val, 20)
-        minHeap.insert(root, 80)
+        result = minHeap.addElement(root, 37)
+        self.assertEqual(result.val, 37)
+        result = minHeap.addElement(root, 24)
+        self.assertEqual(result.val, 24)
+        result = minHeap.addElement(root, 43)
+        self.assertEqual(result.val, 24)
+        minHeap.addElement(root, 70)
+        minHeap.addElement(root, 61)
 
         # Checks preorder
-        odd_preorder_result = minHeap.odd_preorder(root, [])
-        self.assertEqual(odd_preorder_result, [])
+        self.assertEqual(minHeap.preorder(root, []), [24, 43, 50, 61, 37, 70], "Pre-Order does not match")
 
         # Checks postorder
         postorder_result = minHeap.postorder(root, [])
-        self.assertEqual(postorder_result, [70, 50, 80, 30, 20])
+        self.assertEqual(postorder_result, [50, 61, 43, 70, 37, 24], "Post-Order does not match")
+
+        # Checks inorder
+        inorder_result = minHeap.inorder(root, [])
+        self.assertEqual(inorder_result, [50, 43, 61, 24, 70, 37], "In-Order does not match")
+
+        # Checks toArray
+        toarray_result = minHeap.__repr__(root)
+        self.assertEqual(toarray_result, [24, 43, 37, 50, 61, 70])
+
+        # Checks toString
+        tostring_result = minHeap.__str__(root)
+        self.assertEqual(tostring_result, '24, 43, 37, 50, 61, 70', "Strings do not match")
+
+        # Checks OddFilter
+        oddFilter = OddFilter(minHeap.preorder(root, []))
+        oddFilter_result = oddFilter.filter()
+        self.assertEqual(oddFilter_result, [43, 61, 37])
+
+        # Checks OddHeapDecorator
+        oddHeapDecorator_result = OddHeapDecorator(minHeap.preorder(root, []), root)
+        self.assertEqual(oddHeapDecorator_result.__repr__(), [43, 61, 37])
+
+
+    def test_MaxHeap(self):
+        minHeap = Heap(heapType=MaxHeap())
+
+        root = Node(50)
+        result = minHeap.addElement(root, 37)
+        self.assertEqual(result.val, 50)
+        minHeap.addElement(root, -24)
+        minHeap.addElement(root, 43)
+        minHeap.addElement(root, -70)
+        minHeap.addElement(root, 61)
+
+        # Checks postorder
+        postorder_result = minHeap.postorder(root, [])
+        self.assertEqual(postorder_result, [37, 43, 50, -70, -24, 61])
+
+        # Checks preorder
+        self.assertEqual(minHeap.preorder(root, []), [61, 50, 37, 43, -24, -70], "Pre-Order does not match")
+    
+if __name__ == "__main__":
+    unittest.main()
+    
